75884deeedee25c7204b2fef5928317ec72a626d
Gino Lisignoli <gino@gino-debian>
2015-12-17 13:54:39 +1300

    Inital commit

---
+++ i3/volumescript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ $1 = 'up' ]; then
+  amixer -q sset Master,0 1%+ unmute
+  killall -USR1 i3status
+  VOLUME=$(amixer get Master | grep % | head -n 1 | awk '{print $5}')
+  notify-send -t 500 "$VOLUME"
+fi
+if [ $1 = 'down' ]; then
+  amixer -q sset Master,0 1%- unmute
+  killall -USR1 i3status
+  VOLUME=$(amixer get Master | grep % | head -n 1 | awk '{print $5}')
+  notify-send -t 500 "$VOLUME"
+fi
