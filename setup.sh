f274052001ec62dae3664f0d61930aa990934cbf
gino <gino@gino-vm>
2017-03-26 07:45:36 +1300

    added tmuxline and autotmux startup

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 # Backup old files
 echo "Backing up old files to ~/.olddotfiles"
 mkdir ~/.olddotfiles
-mv ~/.irssi ~/.Xdefaults ~/.bashrc ~/.bash_profile ~/.vagrant_ps1 ~/.gitconfig ~/.tmux.conf ~/.vimrc ~/.olddotfiles 2>&1 > /dev/null
+mv ~/.irssi ~/.Xdefaults ~/.bashrc ~/.bash_profile ~/.vagrant_ps1 ~/.gitconfig ~/.tmux.conf ~/.vimrc ~/.olddotfiles > /dev/null 2>&1
 
 # Link files
 ln -s `pwd`/irssi ~/.irssi
@@ -14,4 +14,5 @@ ln -s `pwd`/bash/bash_profile ~/.bash_profile
 ln -s `pwd`/bash/vagrant_ps1 ~/.vagrant_ps1
 ln -s `pwd`/git/gitconfig ~/.gitconfig
 ln -s `pwd`/tmux/tmux.conf ~/.tmux.conf
+ln -s `pwd`/tmux/tmuxline.snapshot ~/.tmuxline.snapshot
 ln -s `pwd`/vimrc/vimrc ~/.vimrc
