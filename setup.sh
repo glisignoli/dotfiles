41559dfed21a2ac39e606e5a72243ff00fce882b
Gino Lisignoli <glisignoli@gmail.com>
2017-11-03 10:32:59 +1300

    Update for submodules

--- setup.sh
+++ setup.sh
@@ -3,22 +3,35 @@
 
 # Backup old files
 echo "Backing up old files to ~/.olddotfiles"
-mkdir ~/.olddotfiles
-mv ~/.irssi ~/.Xdefaults ~/.bashrc ~/.bash_profile ~/.vagrant_ps1 ~/.gitconfig ~/.tmux.conf ~/.vimrc ~/.olddotfiles > /dev/null 2>&1
+mkdir -p ~/.olddotfiles
+mv ~/.irssi \
+  ~/.Xdefaults \
+  ~/.bashrc \
+  ~/.bash_profile \
+  ~/.vagrant_ps1 \
+  ~/.gitconfig \
+  ~/.tmux.conf \
+  ~/.tmuxline.snapshot \
+  ~/.vimrc \
+  ~/.olddotfiles \
+  > /dev/null 2>&1
 
 # Link files
-ln -s `pwd`/irssi ~/.irssi
-ln -s `pwd`/Xdefaults/.Xdefaults ~/.Xdefaults
-ln -s `pwd`/bash/bashrc ~/.bashrc
-ln -s `pwd`/bash/bash_profile ~/.bash_profile
-ln -s `pwd`/bash/vagrant_ps1 ~/.vagrant_ps1
-ln -s `pwd`/git/gitconfig ~/.gitconfig
-ln -s `pwd`/tmux/tmux.conf ~/.tmux.conf
-ln -s `pwd`/tmux/tmuxline.snapshot ~/.tmuxline.snapshot
-ln -s `pwd`/vimrc/vimrc ~/.vimrc
+echo "Linking files"
+ln -sf `pwd`/irssi ~/.irssi
+ln -sf `pwd`/Xdefaults/.Xdefaults ~/.Xdefaults
+ln -sf `pwd`/bash/bashrc ~/.bashrc
+ln -sf `pwd`/bash/bash_profile ~/.bash_profile
+ln -sf `pwd`/bash/vagrant_ps1 ~/.vagrant_ps1
+ln -sf `pwd`/git/gitconfig ~/.gitconfig
+ln -sf `pwd`/tmux/tmux.conf ~/.tmux.conf
+ln -sf `pwd`/tmux/tmuxline.snapshot ~/.tmuxline.snapshot
+ln -sf `pwd`/vimrc/vimrc ~/.vimrc
 
 # Setup bin directory
 mkdir -p ~/bin
 
 # Link binaries
-ln -s `pwd`/tmux/scripts/tmux-ssh.sh ~/bin/tmux-ssh
+echo "Linking Binaries"
+ln -sf `pwd`/tmux/scripts/tmux-ssh.sh ~/bin/tmux-ssh
+ln -sf `pwd`/submodules/bash-git-prompt/gitprompt.sh ~/bin/gitprompt.sh
